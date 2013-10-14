9bda0c6ca577afc3be9482387657a5f6f9330b4d
guoqs <guoqs@funshion.com>
2013-10-14 09:43:19 +0800

    init docs and tests

--- example/urls.py
+++ example/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'table.views.home', name='home'),
+    url(r'^$', 'app.views.people', name='people'),
     # url(r'^table/', include('table.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
