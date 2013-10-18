aa476cf4c138d935f90776f891c09c6315d96b49
guoqs <guoqs@funshion.com>
2013-10-18 17:53:12 +0800

    update example

--- example/app/tables.py
+++ example/app/tables.py
@@ -5,5 +5,5 @@ from table import Table, Column
 
 
 class PersonTable(Table):
-    id = Column(index=True, title=u'序号')
-    name = Column(attr='name', title=u'姓名')
+    id = Column(index=True, title=u'序号', width='50%')
+    name = Column(field='name', title=u'姓名', width='50%')
