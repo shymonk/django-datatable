4d3792a2c85a16f64e740c33dba450a2a02bdbf3
guoqs <guoqs@funshion.com>
2013-10-22 16:38:58 +0800

    fix a bug about column order

--- table/column/base.py
+++ table/column/base.py
@@ -2,11 +2,16 @@
 # coding: utf-8
 
 class Column(object):
+    
+    instance_order = 0
+
     def __init__(self, field=None, index=False, attrs={}, header=None, header_attrs={}):
         self.field = field
         self.index = index
         self.attrs = attrs
         self.header = ColumnHeader(text=header, attrs=header_attrs)
+        self.instance_order = Column.instance_order
+        Column.instance_order += 1
 
     def render_attrs(self):
         """ Render attributes of <td> to html.
