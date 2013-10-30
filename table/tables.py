6471fb3bd974ae2aeb41a0d0d21ce0aa327817cf
guoqs <guoqs@funshion.com>
2013-10-30 18:07:19 +0800

    rename ext_button -> ext_button_text

--- table/tables.py
+++ table/tables.py
@@ -37,7 +37,7 @@ class BaseTable(object):
         html = ''
         if self.opts.ext_button_link:
             html = '<a href="%s" target="_blank" class="btn btn-default">%s</a>' % \
-                (self.opts.ext_button_link, self.opts.ext_button)
+                (self.opts.ext_button_link, self.opts.ext_button_text)
         return mark_safe(html)
 
 class TableOptions(object):
@@ -69,7 +69,7 @@ class TableOptions(object):
         self.page_prev = getattr(options, 'page_prev', '上一页')
         self.page_next = getattr(options, 'page_next', '下一页')
 
-        self.ext_button = getattr(options, 'ext_button', u'添加记录 +')
+        self.ext_button_text = getattr(options, 'ext_button_text', u'添加记录 +')
         self.ext_button_link = getattr(options, 'ext_button_link', None)
 
 
