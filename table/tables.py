a41dd05c522980c0fc189c123a946ae1f88e1d32
guoqs <guoqs@funshion.com>
2013-11-04 15:52:52 +0800

    fix bug about displaying column attrs

--- table/tables.py
+++ table/tables.py
@@ -27,6 +27,9 @@ class BaseTable(object):
     def rows(self):
         rows = []
         for obj in self.data:
+            # Binding object to each column of each row, so that
+            # data structure for each row is organized like this:
+            # { boundcol0: td, boundcol1: td, boundcol2: td }
             row = SortedDict()
             columns = [BoundColumn(obj, col) for col in self.columns]            
             for col in columns:
