1b4c8d81f4a489d5106d6376dfc6af2288afc6c9
shymonk <hellojohn201@gmail.com>
2014-01-23 23:31:00 +0800

    Refactoring sequencecolumn

--- table/columns/sequencecolumn.py
+++ table/columns/sequencecolumn.py
@@ -4,8 +4,30 @@
 from .base import Column
 
 class SequenceColumn(Column):
-    def __init__(self, *args, **kwargs):
-        super(SequenceColumn, self).__init__(*args, **kwargs)
+    def __init__(self, field, headers, **kwargs):
+        self.headers = headers
+        super(SequenceColumn, self).__init__(field, **kwargs)
 
-    def extract(self):
-        pass
+    @property
+    def columns(self):
+        return [self.get_column(key) for key in self.__len__()]
+
+    def __len__(self):
+        return len(self.headers)
+
+    def __getitem__(self, key):
+        return self.columns[key]
+
+    def __setitem__(self, key, value):
+        self.columns[key] = value
+
+    def get_column(self, key):
+        return Column(field=self.get_field(key),
+                      header=self.get_header(key),
+                      **kwargs)
+
+    def get_field(self, key):
+        return ".".join([self.field, str(key)])
+
+    def get_header(self, key):
+        return self.headers[key]
--- table/tests/test_columns.py
+++ table/tests/test_columns.py
@@ -3,17 +3,55 @@
 
 from datetime import date
 from django.test import TestCase
-from table.columns import MonthsColumn, WeeksColumn, DaysColumn
+from table.columns.base import Column, BoundColumn, ColumnHeader
+from table.columns.sequencecolumn import SequenceColumn
+from table.columns.calendarcolumn import DaysColumn, WeeksColumn, MonthsColumn
+
+
+class BaseColumntestCase(TestCase):
+    def test_create_instance(self):
+        column = Column("field")
+
+        self.assertEqual(column.field, "field")
+        self.assertEqual(column.attrs, {})
+
+        self.assertTrue(column.sortable, True)
+        self.assertTrue(column.searchable, True)
+        self.assertTrue(column.safe, True)
+        self.assertTrue(column.visible, True)
+        self.assertTrue(column.space, True)
+
+    def test_column_header(self):
+        column = Column("field", "A",header_attrs={"id": "col-id"},
+                        header_row_order=1)
+        self.assertEqual(column.header.text, "A")
+        self.assertEqual(column.header.attrs, 'id="col-id"')
+        self.assertEqual(column.header.row_order, 1)
+
+    def test_render(self):
+        class Foo():
+            def __init__(self, foo):
+                self.foo = foo
+        f = Foo("bar")
+        column = Column("foo")
+        self.assertEqual(column.render(f), "bar")
 
 
 class SequenceColumnTestCase(TestCase):
     def setUp(self):
-        pass
+        self.column = SequenceColumn("foo", headers=["A", "B", "C"])
 
-    def test_column_headers(self):
-        pass
+    def test_create_instance(self):
+        self.assertEqual(self.column.headers, ["A", "B", "C"])
 
-    def test_columns_count(self):
+    def test_length(self):
+        self.assertEqual(len(self.column), 3)
+
+    def test_getitem(self):
+        col = self.column[0]
+        self.assertEqual(col.header)
+
+    def test_extend_columns(self):
         pass
 
     def test_get_column_field(self):
