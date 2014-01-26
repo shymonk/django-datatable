43edfe8c4eb3f069e87ab03a095c75335ad56c17
guoqs <hellojohn201@gmail.com>
2014-01-26 10:01:30 +0800

    Update calendarcolumn

--- table/columns/calendarcolumn.py
+++ table/columns/calendarcolumn.py
@@ -16,22 +16,22 @@ class DaysColumn(SequenceColumn):
 
 
 class WeeksColumn(SequenceColumn):
-    def __init__(self, field, start_date, end_date, week_name=None, **kwargs):
-        week_name = week_name or calendar.day_abbr
+    WEEK_NAME = calendar.day_abbr
 
+    def __init__(self, field, start_date, end_date, **kwargs):
         days = (end_date - start_date).days + 1
         dates = [start_date + timedelta(day) for day in range(days)]
-        format_dates = [week_name[date.weekday()] for date in dates]
+        format_dates = [self.WEEK_NAME[date.weekday()] for date in dates]
         super(WeeksColumn, self).__init__(field, format_dates, **kwargs)
 
 
 class MonthsColumn(SequenceColumn):
-    def __init__(self, field, start_date, end_date, month_name=None, **kwargs):
-        month_name = month_name or calendar.month_name[1:]
+    MONTH_NAME = calendar.month_name[1:]
 
+    def __init__(self, field, start_date, end_date, month_name=None, **kwargs):
         delta_year = end_date.year - start_date.year
         delta_month = end_date.month - start_date.month
         months = delta_year * 12 + delta_month + 1
-        format_dates = [month_name[(start_date.month + month - 1) % 12]
+        format_dates = [self.MONTH_NAME[(start_date.month + month - 1) % 12]
                         for month in range(months)]
         super(MonthsColumn, self).__init__(field, format_dates, **kwargs)
