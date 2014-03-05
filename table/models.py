7351351a02bc0c291bde87803b6546d0395c8e60
shymonk <hellojohn201@gmail.com>
2014-03-05 08:48:55 +0800

    Update test case

--- table/models.py
+++ table/models.py
@@ -1,3 +1,11 @@
+# coding: utf-8
+
+""" Model for test.
+"""
+
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
--- table/tests/test_views.py
+++ table/tests/test_views.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from uuid import uuid4
 from django.test import Client, TestCase
 from django.core.urlresolvers import reverse
 from django.utils import simplejson as json
 from table.views import FeedDataView
 from table.forms import QueryDataForm
+from table.models import Person
+from table.columns import Column
+from table import Table
+
+
+class TestTable(Table):
+    id = Column('id', header='#')
+    name = Column('name', header='NAME')
+
+    class Meta:
+        model = Person
+        ajax = True
 
 
 class FeedDataViewTestCase(TestCase):
     def setUp(self):
+        self.table = TestTable()
         self.client = Client()
-        self.url = reverse("feed_data")
-        self.set_queryset_method()
-
-    def set_queryset_method(self):
-        def get_queryset(self):
-            object_list = [
-                {"name": "A", "age": 18},
-                {"name": "B", "age": 35},
-                {"name": "C", "age": 70},
-            ]
-            return object_list
-        FeedDataView.get_queryset = get_queryset
-
-    def test_api_basic(self):
-        payload = {
+        self.url = reverse("feed_data", args=(self.table.token,))
+        self.payload = {
             "sEcho": "1",
             "iColumns": 2,
             "iDisplayStart": 0,
@@ -34,22 +35,38 @@ class FeedDataViewTestCase(TestCase):
             "bRegex": False,
             "iSortingCols": 1
         }
-        response = self.client.get(self.url, payload)
-        status_code = response.status_code
-        data = json.loads(response.content)
-        self.assertEqual(status_code, 200)
 
+        Person.objects.create(id=1, name="Tom")
+        Person.objects.create(id=2, name="Jerry")
+
+    def test_api_basic(self):
+        response = self.client.get(self.url, self.payload)
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.content)
         expect_data = {
             "sEcho": "1",
-            "iTotalRecords": 3,
-            "iTotalDisplayRecords": 3,
-            "aaData": [["A", 18], ["B", 35], ["C", 70]]
+            "iTotalRecords": 2,
+            "iTotalDisplayRecords": 2,
+            "aaData": [[1, "Tom"], [2, "Jerry"]]
             }
         self.assertEqual(data, expect_data)
 
     def test_api_search(self):
-        pass
+        self.payload["sSearch"] = "T"
+
+        response = self.client.get(self.url, self.payload)
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.content)
+        expect_data = {
+            "sEcho": "1",
+            "iTotalRecords": 2,
+            "iTotalDisplayRecords": 1,
+            "aaData": [[1, "Tom"]]
+            }
+        self.assertEqual(data, expect_data)
+        
 
     def test_api_sort(self):
         pass
-
