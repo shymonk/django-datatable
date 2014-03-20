25a5e9cb5cec48f1cccbf414d847677b09e2e5e1
guoqs <hellojohn201@gmail.com>
2014-03-20 19:12:03 +0800

    Extern API method for datatable

--- table/static/js/bootstrap.dataTables.js
+++ table/static/js/bootstrap.dataTables.js
@@ -23,6 +23,26 @@ $.extend( $.fn.dataTableExt.oStdClasses, {
     "sLengthSelect": "form-control input-sm"
 } );
 
+/* API method to get hidden rows */
+$.fn.dataTableExt.oApi.fnGetHiddenNodes = function ( oSettings )
+{
+    /* Note the use of a DataTables 'private' function thought the 'oApi' object */
+    var anNodes = this.oApi._fnGetTrNodes( oSettings );
+    var anDisplay = $('tbody tr', oSettings.nTable);
+      
+    /* Remove nodes which are being displayed */
+    for ( var i=0 ; i<anDisplay.length ; i++ )
+    {
+        var iIndex = jQuery.inArray( anDisplay[i], anNodes );
+        if ( iIndex != -1 )
+        {
+            anNodes.splice( iIndex, 1 );
+        }
+    }
+      
+    /* Fire back the array to the caller */
+    return anNodes;
+};
 
 /* API method to get paging information */
 $.fn.dataTableExt.oApi.fnPagingInfo = function ( oSettings )
