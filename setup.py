da87f9b7e00076db5695982a3c76f8731f1543a2
shymonk <hellojohn201@gmail.com>
2014-06-09 20:44:08 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-table',
+    name='django-datatable',
     version='0.1.2',
     packages=find_packages(exclude=['test*', 'example*']),
     include_package_data=True,
